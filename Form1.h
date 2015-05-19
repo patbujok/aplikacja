af02a82434f3d94d94c6b43308c372b311ee5c47
patbujok <pat.bujok@gmail.com>
2015-05-19 21:03:28 +0200

    Dodanie przycisku "+" do drugiego okna.

--- Form1.h
+++ Form1.h
@@ -94,6 +94,7 @@ namespace WindowsFormApplication1 {
 			this->button2->TabIndex = 2;
 			this->button2->Text = L"miniKalkulator";
 			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &Form1::button2_Click);
 			// 
 			// button3
 			// 
@@ -125,6 +126,21 @@ namespace WindowsFormApplication1 {
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
 		Close();
 	}
-	};
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+		Form^ okno = gcnew Form;
+		okno->Show();
+
+		okno->Width = 300;
+		okno->Height = 300;
+		okno->Text = "miniKalkulator";
+		okno->BackColor = System::Drawing::Color::NavajoWhite;
+
+		Button^ plus = gcnew Button;
+		plus->Location = Point(40, 70);
+		plus->Text = L"+";
+		plus->BackColor = System::Drawing::Color::Snow;
+		okno->Controls->Add(plus);
+	}
+};
 }
 
