b3270d34c7ec36bb1aad1122db7c688cdc93e216
patbujok <pat.bujok@gmail.com>
2015-05-19 17:52:00 +0200

    Dodanie etykiety i dwóch przycisków.

--- Form1.h
+++ Form1.h
@@ -35,6 +35,9 @@ namespace WindowsFormApplication1 {
 			}
 		}
 	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button3;
 	protected:
 
 	private:
@@ -51,6 +54,9 @@ namespace WindowsFormApplication1 {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -66,16 +72,53 @@ namespace WindowsFormApplication1 {
 			this->button1->UseVisualStyleBackColor = false;
 			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Bookman Old Style", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label1->Location = System::Drawing::Point(160, 67);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(166, 21);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Co chcesz robiæ\?";
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::Snow;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Bookman Old Style", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->button2->Location = System::Drawing::Point(57, 151);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(140, 35);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"miniKalkulator";
+			this->button2->UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(286, 151);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(140, 35);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"button3";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::LightBlue;
 			this->ClientSize = System::Drawing::Size(484, 312);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
 			this->Name = L"Form1";
-			this->Text = L"Form1";
+			this->Text = L"Aplikacja";
+			this->TopMost = true;
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
